e5ab9826163a5b908576f4923c36b8dbfc9fe231
jtroubleK <149900298+jbellido89@users.noreply.github.com>
2024-11-05 22:55:26 +0100

    Añadir componente de lista de lugares y modal para ver en el mapa

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Routes>
           <Route path='/' element={<Users />} />
+          <Route path='/:userId/places' element={<UserPlaces />} />
           <Route path='/places/new' element={<NewPlace />} />
           <Route path='*' element={<Navigate to='/' />} />
         </Routes>
